6d10b98f01da73dcb13e5b9fb8f7ca32096eae95
lobinda <lobinda123@gmail.com>
2026-05-24 23:00:34 +0800

    註冊+redirect

---
+++ front-end/assets/js/index.js
@@ -0,0 +1,85 @@
+
+
+const LIFF_ID = "2010150440-sbW0urAy";
+
+
+const API_BASE_URL =
+    "https://1035-59-124-220-148.ngrok-free.app";
+
+async function init() {
+
+    await liff.init({
+        liffId: LIFF_ID
+    });
+
+    if (!liff.isLoggedIn()) {
+
+        liff.login();
+
+        return;
+    }
+
+    const profile =
+        await liff.getProfile();
+
+    const lineUserId =
+        profile.userId;
+
+    const response =
+        await fetch(
+            `${API_BASE_URL}/api/activity/current/${lineUserId}`
+        );
+
+    if (!response.ok) {
+
+        window.location.href =
+            "./pages/register.html";
+
+        return;
+    }
+
+    const data =
+        await response.json();
+
+    /*
+        status:
+
+        NONE
+        ACTIVE
+        COMPLETED
+        REWARDED
+    */
+
+    switch (data.status) {
+
+        case "ACTIVE":
+
+            window.location.href =
+                "./pages/progress.html";
+
+            break;
+
+        case "COMPLETED":
+
+            window.location.href =
+                "./pages/reward.html";
+
+            break;
+
+        case "REWARDED":
+
+            window.location.href =
+                "./pages/progress.html";
+
+            break;
+
+        default:
+
+            window.location.href =
+                "./pages/register.html";
+
+            break;
+    }
+}
+
+init();
---
+++ front-end/assets/js/register.js
@@ -0,0 +1,47 @@
+
+import {
+    initLiff
+} from "..liff/liff-init.js";
+
+const API_BASE_URL =
+    "https://1035-59-124-220-148.ngrok-free.app";
+
+
+async function init() {
+
+    const profile =
+        await initLiff();
+
+    if (!profile) {
+        return;
+    }
+
+    console.log(profile);
+
+    const userId =
+        profile.userId;
+
+    const response =
+        await fetch(
+            `${API_BASE_URL}/api/activity/current/${userId}`
+        );
+
+    const data =
+        await response.json();
+
+    if (data.hasActivity) {
+
+        window.location.href =
+            "/pages/progress.html";
+
+        return;
+    }
+
+    document.getElementById(
+        "name"
+    ).value =
+        profile.displayName;
+}
+
+init();
+
