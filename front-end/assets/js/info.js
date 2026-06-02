26d90d6d56407056587f5842c777e1c1566ec4fc
lobinda <lobinda123@gmail.com>
2026-06-02 13:37:58 +0800

    fix: 修正liff參數

--- front-end/assets/js/info.js
+++ front-end/assets/js/info.js
@@ -2,6 +2,10 @@ import {
     initLiff
 } from "../liff/liff-init.js";
 
+const API_BASE_URL =
+    "https://9f4d-59-124-220-148.ngrok-free.app";
+let html5QrCode = null;
+
 async function init() {
 
     const profile =
