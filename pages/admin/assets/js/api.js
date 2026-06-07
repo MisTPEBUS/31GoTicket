499676ec40e1610748df9ad27e25589694581488
MisTPEBUS <MisTPEBUS@users.noreply.github.com>
2026-06-07 04:23:54 +0000

    deploy: e8d815fa4f35307c5dbf4ded55b2aa59788f187f

---
+++ pages/admin/assets/js/api.js
@@ -0,0 +1,29 @@
+'use strict';
+//member
+const UserApi = {
+    getAdminUserByActiveCode(activeCode) {
+        return apiFetch(
+            `/api/user/admin/info/${encodeURIComponent(activeCode)}`
+        );
+    },
+
+    updateUserRole(activeCode, role) {
+        return apiFetch(
+            `/api/user/admin/info/${encodeURIComponent(activeCode)}/${encodeURIComponent(role)}`,
+            {
+                method: 'POST'
+            }
+        );
+    }
+};
+
+//import-order
+
+const OrdersApi = {
+    importOrders(payload) {
+        return apiFetch('/api/admin/orders/import', {
+            method: 'POST',
+            body: payload
+        });
+    }
+};
