ff65581aeb8eb063b9d1f5eb696cd28cbd14d544
MisTPEBUS <MisTPEBUS@users.noreply.github.com>
2026-06-07 18:36:31 +0000

    deploy: b61838a7c566de5f147beea3af9be616eb595741

--- pages/admin/assets/js/api-client.js
+++ pages/admin/assets/js/api-client.js
@@ -3,6 +3,7 @@
 /* const API_BASE_URL =
     window.API_BASE_URL || 'https://9f4d-59-124-220-148.ngrok-free.app';
  */
+API_BASE_URL = 'https://9f4d-59-124-220-148.ngrok-free.app';
 async function apiFetch(path, options = {}) {
     const url = `${API_BASE_URL}${path}`;
 
